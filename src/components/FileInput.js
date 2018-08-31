45dcea4c84da435797e0c07a281a6f1f18dc9dc4
Hugo González <pinelo93@gmail.com>
2018-08-31 10:27:25 -0500

    Fixes capitalization error

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ipfs from '../lib/ipfs';
-import Spinner from './spinner';
+import Spinner from './Spinner';
 import { type File } from '../lib/types';
 import {
   onFileDataChange,
